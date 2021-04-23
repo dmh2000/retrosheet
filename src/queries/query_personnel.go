1e756067676054e71bb6db6d99869a585db758ab
David Howard <dmh2000@gmail.com>
2021-04-22 21:38:54 -0700

    missed a file save, no change to function

--- src/queries/query_personnel.go
+++ src/queries/query_personnel.go
@@ -7,59 +7,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// // GetPersonnelByKey find personnel matching a single key
-// // this is the full inline version
-// func GetPersonnelByKey(uri string, key string, value string) ([]jsontypes.Person, error) {
-// 	var err error
-// 	var client *mongo.Client
-
-// 	client, err = mongo.NewClient(options.Client().ApplyURI(uri))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
-
-// 	err = client.Connect(ctx)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer client.Disconnect(ctx)
-
-// 	// get the personnel collection
-// 	coll := client.Database("retrosheet").Collection("personnel")
-
-// 	// find documents that have the key "abbr" with the specified value
-// 	cursor, err  := coll.Find(ctx, bson.D{{Key:key, Value:value}} )
-// 	defer cursor.Close(ctx)
-
-// 	// insertion failed
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// some other fail, auth error can do this
-// 	if cursor == nil {
-// 		return nil, errors.New("no result : authorization?")
-// 	}
-
-// 	// decode all matching records
-// 	var people []jsontypes.Person
-// 	for cursor.Next(ctx) {
-// 		var p jsontypes.Person
-
-// 		// decode into a team
-// 		err = cursor.Decode(&p)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 		// add to list
-// 		people = append(people,p)
-// 	}
-// 	return people, nil
-// }
-
 // =================================================================
 // The following version uses the refactored database query
 // handler QueryDb (in query_util.go)
